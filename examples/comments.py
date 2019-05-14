a8a65e2d5576e605c0b69edccfa1d5b048e5b47c
Scott Lusk <slusk@capaxglobal.com>
2019-05-14 16:46:37 -0400

    add example of comments

---
+++ examples/comments.py
@@ -0,0 +1,13 @@
+# this is a single line comment
+print("hello")
+
+print("hello world")  # exmaple single line comment after code 
+
+# example using multi line comment
+def add_student(name, student_id=332):
+    """
+    Adds the student to the student list.
+    :param name: string - stduent name
+    :param student_id: integer - optioanl student ID
+    """
+    student = "Mark"
